0c0eb28a7fd1cd6cc54bf52461dbee89c396b714
newspaceracer <newspaceracer@users.noreply.github.com>
2026-08-19 15:54:50 +0000

    deploy: b71b5300a82a9c96bf70a40fc35f0243381eaadc

--- _astro/esa-text-field.astro_astro_type_script_index_0_lang.CSXfiwwm.js
+++
@@ -1,1 +0,0 @@
-import"./esa-text-field.T8WF7xnN.js";import"./lit-element.C8p3bJxG.js";const t=document.getElementById("tf-form");t?.addEventListener("submit",e=>{e.preventDefault();const n=new FormData(t);document.getElementById("tf-out").textContent="contact = "+JSON.stringify(n.get("contact"))});
